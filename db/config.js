b73e726a5b48890fcdf46e6323b19ef9805d0ce3
jackmqiu <jackqiu2016@gmail.com>
2017-11-07 15:54:47 -0800

    docker compose working with node app and redis containers

--- db/config.js
+++ db/config.js
@@ -1,5 +1,22 @@
 module.exports = {
-  redis_host: '127.0.0.1',
-  redis_port: 16379,
+  redis_host: 'redis',
+  redis_port: 6379,
   queue_size: 100
 }
+
+/* Things tried for docker
+0.0.0.0
+16379
+0.0.0.0
+6379
+127.0.0.1
+16379
+127.0.0.1
+6379
+http://localhost:16379
+timder-redis
+16379
+redis
+6379
+(with and without timder-redis container stopped)
+*/
